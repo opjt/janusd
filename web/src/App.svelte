a2da5c6ab3a4ab098e59e1c669bea90d8ca5ae93
opjt <jtpark1957@gmail.com>
2026-04-11 11:34:05 +0900

    feat: Add web UI

---
+++ web/src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import './app.css'
+  import Dashboard from './lib/Dashboard.svelte'
+</script>
+
+<Dashboard />
---
+++ web/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    svelte(),
+  ],
+  server: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+    },
+  },
+})
